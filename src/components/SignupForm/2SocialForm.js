27311c116abe5ce8c45f1af4be759f2d4c923856
olympiawoj <owojcik7@gmail.com>
2021-04-07 20:37:26 -0400

    Forms and Validation

--- src/components/SignupForm/2SocialForm.js
+++ src/components/SignupForm/2SocialForm.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import { useHistory } from 'react-router-dom'
 
-export default function SocialForm(){
+export default function SocialForm() {
+    const { register, handleSubmit, formState: { errors } } = useForm()
+    const history = useHistory()
+
+    function onSubmit(data) {
+        console.log(data)
+        history.push('/review')
+    }
+
     return (
-        <form>
-            I am the social form
-            <input type="submit" value="Next"/>
+        <form onSubmit={handleSubmit(onSubmit)} noValidate>
+            <h2>How can we find you on social?</h2>
+
+            <input type="text" name="twitter" placeholder="What's your Twitter?" {...register("twitter", { required: true })} />
+            <p>{errors.twitter && 'Twitter is required.'}</p>
+            <input type="email" name="facebook" placeholder="What's your Facebook?" {...register("facebook", { required: true})} />
+            <p>{errors.facebook && 'Facebook is required.'}</p>
+            <input type="submit" value="Next" />
         </form>
     )
 }
